ee82e7609bf7c16c259e02f92ed4cabd266ae46b
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-02-11 18:43:02 -0800

    safety commit working on card comparison

--- js/app.js
+++ js/app.js
@@ -10,12 +10,14 @@ const game = {
   //objects for player and CPU
   player: {
     dealtCards: [],
-    score: 0
+    score: 0,
+    usedCards: []
   },
   //objects for CPU
   CPU: {
     dealtCards: [],
-    score: 0
+    score: 0,
+    usedCards: []
   },
 
   currentRound: 1,
@@ -29,7 +31,7 @@ const game = {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-    console.log(cards);
+    // console.log(cards);
   },
 
   // deal 26 cards to each player
@@ -38,33 +40,55 @@ const game = {
     for (let i = 0; i < 26; i++) {
       this.player.dealtCards.push(cards.pop());
       this.CPU.dealtCards.push(cards.pop());
-      console.log(cards[i]);
+      // console.log(cards[i]);
     }
   },
 
-  dealCard: function(){
+  // payer clicks on the deck in order to choose the last card on the array of 26 cards
+  //cpu clicks on the deck in order to choose the last card on the array of 26 cards
 
+  //prints to array
+  playCard: function(card) {
+    //deal out last card
+    let playerLastCard = this.player.dealtCards.pop();
+    let CPULastCard = this.CPU.dealtCards.pop();
+    console.log(`player card: ${playerLastCard.name}`); //
+    console.log(`CPU Card: ${CPULastCard.name}`);
+
+    console.log("Compare values!");
+    this.compareCards(playerLastCard, CPULastCard);
   },
 
-  
+  //figure out which card has higher value
+  compareCards: function(playerCard, CPUCard) {
+    if (playerCard === CPUCard) {
+      console.log("Time to Battle!!!");
+    } else if (playerCard < CPUCard) {
+      this.CPU.usedCards.push(playerCard && CPUCard);
+      console.log("oh no!");
+    } else if (playerCard > CPUCard) {
+      this.player.usedCards.push(playerCard && CPUCard);
+      console.log("You're doing great!");
+    }
+  },
 
   gameplayLoop: function() {
     this.shuffle(cards);
     this.player.dealtCards;
     this.CPU.dealtCards;
+    this.playCard();
     // console.log(cards);
   }
 };
 // console.log(game.shuffle(cards))
 game.shuffle(cards);
 game.splitDeal(cards);
-console.log(game.player.dealtCards);
+// console.log(game.player.dealtCards);
 console.log(game.CPU.dealtCards);
-console.log(game.player);
+// console.log(game.player);
 
-// payer clicks on the deck in order to choose the last card on the array of 26 cards
-//cpu clicks on the deck in order to choose the last card on the array of 26 cards
-//figure out which card has higher value
+game.playCard();
+
 //the card with highest value will will both cards and put them in another array (used cards)
 //*******show the player whether they won or lost that round and the score
 //get rid of both played cards
